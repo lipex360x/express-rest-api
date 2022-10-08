421cd28e7410933d866ba66b9ae0e77690f70d74
Felipe Bambirra <lipex360@gmail.com>
2022-10-08 15:49:56 -0300

    project updated

--- src/app.ts
+++ src/app.ts
@@ -6,4 +6,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-export default app
+export { app }
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,33 @@
-import app from './app'
+import { Application } from 'express'
+
+import { app } from './app'
 
 class Server {
-  execute(): void {
-    const api = app.listen('3333', () => {
+  private readonly appServer: Application
+
+  constructor() {
+    this.appServer = app
+
+    this.start()
+    this.stop()
+  }
+
+  private start(): void {
+    this.appServer.listen('3333', () => {
       console.info('server started on port 3333')
     })
+  }
 
+  private stop(): void {
     const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT']
 
     exitSignals.forEach((signal: string) => {
       process.on(signal, () => {
         console.info(`\nClosing server connections due an ${signal} command...`)
-        api.close()
+        process.exit()
       })
     })
   }
 }
 
-export default new Server().execute()
+export default new Server()
