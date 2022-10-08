10a158d1061d460d2efc2c5f7d52c6ddf719b912
Felipe Borges <lipex360@gmail.com>
2022-10-08 14:59:34 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import app from './app'
 
-app.listen(3333, () => {
-  console.log('API REST started\n')
-})
+class Server {
+  execute(): void {
+    const api = app.listen('3333', () => {
+      console.info('server started on port 3333')
+    })
+
+    const exitSignals = ['SIGINT', 'SIGTERM', 'SIGQUIT']
+
+    exitSignals.forEach((signal: string) => {
+      process.on(signal, () => {
+        console.info(`\nClosing server connections due an ${signal} command...`)
+        api.close()
+      })
+    })
+  }
+}
+
+export default new Server().execute()
